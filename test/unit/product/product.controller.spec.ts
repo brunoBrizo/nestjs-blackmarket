86dd6b7131d051d5ad4ad5f29b3385398168006e
Bruno Brizolara <bbrizolara7@gmail.com>
2022-12-27 12:31:33 -0300

    Add products CRUD tests

---
+++ test/unit/product/product.controller.spec.ts
@@ -0,0 +1,81 @@
+import { Test, TestingModule } from '@nestjs/testing';
+import { faker } from '@faker-js/faker';
+import { ProductController } from '@controllers/product';
+import { CreateProductDto, UpdateProductDto } from '@dtos/product';
+import { Product } from '@entities/product';
+import { ProductService } from '@services/product';
+
+describe('ProductController', () => {
+  let productController: ProductController;
+
+  const createProductDto: CreateProductDto = {
+    name: faker.commerce.productName(),
+    description: faker.commerce.productDescription(),
+    price: parseFloat(faker.commerce.price()),
+    stock: parseInt(faker.random.numeric(2))
+  };
+
+  const updateProductDto: UpdateProductDto = {
+    name: faker.commerce.productName(),
+    description: faker.commerce.productDescription(),
+    price: parseFloat(faker.commerce.price()),
+    stock: parseInt(faker.random.numeric(2))
+  };
+
+  const mockProduct: Product = {
+    id: faker.datatype.uuid(),
+    name: faker.commerce.productName(),
+    description: faker.commerce.productDescription(),
+    price: parseFloat(faker.commerce.price()),
+    stock: parseInt(faker.random.numeric(2)),
+    created_at: faker.date.recent(),
+    updated_at: faker.date.recent()
+  };
+
+  const mockProductService = {
+    createProduct: jest.fn().mockResolvedValue(mockProduct),
+    updateProduct: jest.fn().mockResolvedValue(mockProduct),
+    deleteProduct: jest.fn().mockResolvedValue(undefined)
+  };
+
+  beforeAll(async () => {
+    const module: TestingModule = await Test.createTestingModule({
+      controllers: [ProductController],
+      providers: [ProductService]
+    })
+      .overrideProvider(ProductService)
+      .useValue(mockProductService)
+      .compile();
+
+    productController = module.get<ProductController>(ProductController);
+  });
+
+  it('should be defined', () => {
+    expect(productController).toBeDefined();
+  });
+
+  it('should create and get a product /POST / ', async () => {
+    const result: Product = await productController.createProduct(
+      createProductDto
+    );
+
+    expect(result).not.toBeNull();
+    expect(result).toEqual(mockProduct);
+  });
+
+  it('should update a product /PUT /:id ', async () => {
+    const result: Product = await productController.updateProduct(
+      mockProduct.id,
+      updateProductDto
+    );
+
+    expect(result).not.toBeNull();
+    expect(result).toEqual(mockProduct);
+  });
+
+  it('should delete a product /DELETE /:id ', async () => {
+    await expect(productController.deleteProduct(mockProduct.id)).resolves.toBe(
+      undefined
+    );
+  });
+});
